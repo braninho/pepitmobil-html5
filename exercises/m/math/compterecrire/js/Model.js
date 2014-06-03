6a2a7005852d164bfc7916b562a976c568202820
braninho <lefebure.brian@gmail.com>
2014-06-03 14:47:36 +0200

    nouveau

--- exercises/m/math/compterecrire/js/Model.js
+++ exercises/m/math/compterecrire/js/Model.js
@@ -17,12 +17,55 @@ m.math.compterecrire.Model = function () {
     this.reset = function() {
     };
 
+    this.getNbPommes = function(){
+        return nbPommes;
+    }
+
+    this.setNbPommes = function(nbPomme){
+        this.nbPommes = nbPomme;
+    }
+
+    this.getCanvas_x = function(){
+        return canvas_x;
+    }
+
+    this.getCanvas_y = function(){
+        return canvas_y;
+    }
+
+    this.getTab = function(){
+        return tab;
+    }
+
 // private methods
     var init = function() {
+        tab = new Array();
+        console.log(nbPommes);
+        for(var i=1;i<=nbPommes;i++){
+            do{
+                var canvas_x = Math.random();
+                //var canvas_x = parseInt(Math.floor(Math.random()*parseInt(canvas[0].width)-52));
+            }while(canvas_x+80/300>1);
+            console.log("x "+i+" : "+canvas_x);
+
+            do{
+                var canvas_y = Math.random();
+                //var canvas_y = parseInt(Math.floor(Math.random()*parseInt(canvas[0].height)-41));
+            }while(canvas_y+50/150>1);
+            console.log("y "+i+" : "+canvas_y);
+
+            tab.push({x1 : canvas_x, y1 : canvas_y, x2 : canvas_x+80/300, y2 : canvas_y+50/150});
+        }
     };
+
 
 // private attributes
     var okResult;
+    var canvas_x;
+    var canvas_y;
+    var nbPommes = 5;
+    var tab;
+
 
     init();
 };
--- exercises/m/math/compterecrire/js/View.js
+++ exercises/m/math/compterecrire/js/View.js
@@ -22,22 +22,93 @@ m.math.compterecrire.View = function (mdl, div) {
         if (model.isOkResult()) {
             module.next();
         }
-
     };
 
 // private methods
-    var init_div = function (view) {
-        var button;
+    var affichagePommes  = function(canvas){
+        var tab = model.getTab();
+        var context = canvas[0].getContext("2d");
+        console.log(model.getTab().length);
+        for(var i=1;i<=model.getTab().length;i++){
 
-        button = $('<a/>', {
+            var img = new Image();
+            img.src = 'exercises/m/math/compterecrire/img/pommes.png';
+
+            var x = Math.floor(tab[i].x1);//(model.getTab().x1)*parseInt(canvas.width));
+            var y = Math.floor(tab[i].y1);//*parseInt(canvas.height));
+
+            console.log();
+            console.log(x);
+            console.log(y);
+
+            context.drawImage(img, x, y, 80, 50);
+            context.stroke();
+        }
+    };
+
+    var init_div = function (view) {
+        operation(view);
+    };
+
+
+    var operation = function(view){
+        //md et lg device
+        var BigCadre_md_lg = $('<div/>',{
+            class:'visible-md visible-lg',
+            id:'BigCadre',
+            style:'background:url("exercises/m/math/compterecrire/img/fond_complet.png") no-repeat center; min-height:654px;'
+        });
+
+        //cadre pour les pommes
+        var cadre=$('<canvas/>',{
+            class:'visible-md visible-lg',
+            id:'cadre',
+            style:' height:330px; width:430px; margin-left:140px;float:left;margin-top:80px;position:relative'
+        });
+        cadre.appendTo(BigCadre_md_lg);
+
+        affichagePommes(cadre);
+
+        //cadres avec les input
+        var smallCadre_md_lg = $('<div/>',{
+            class:'visible-md visible-lg',
+            id:'smallCadre',
+            style:'float:right; background:rgba(21,21,21,0.5); color:white; padding: 5px; margin-right:75px ; border-radius : 5px;'
+        });
+
+        var textBefore_md_lg=$('<p/>',{
+            class:'visible-md visible-lg',
+            id:'textBefore',
+            html:'J\'ai récolté '
+        });
+        textBefore_md_lg.appendTo(smallCadre_md_lg);
+
+        var input_md_lg = $('<input/>',{
+            class:'visible-md visible-lg',
+            id:'input',
+            placeholder:'Nombre de pommes'
+        });
+        input_md_lg.appendTo(smallCadre_md_lg);
+
+        var textAfter_md_lg = $('<p/>',{
+            class:'visible-md visible-lg',
+            id:'textBefore',
+            html:'pommes'
+        });
+        textAfter_md_lg.appendTo(smallCadre_md_lg);
+
+        var button_md_lg = $('<a/>', {
             href: '#',
-            class: 'btn btn-lg btn-warning active',
+            class: 'btn btn-md btn-lg btn-warning active',
             id: 'valid',
             role: 'button',
-            html: 'valid'
+            html: 'Valider'
         });
-        button.appendTo(div);
-    };
+        button_md_lg.appendTo(smallCadre_md_lg);
+
+        smallCadre_md_lg.appendTo(BigCadre_md_lg);
+        BigCadre_md_lg.appendTo(div);
+    }
 
 // private attributes
     var module;
